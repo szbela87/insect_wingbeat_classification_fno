3e0d9095c837b6089b152e2a7b917e00c28f5fea
szbela87 <75852587+szbela87@users.noreply.github.com>
2023-08-18 16:37:17 +0200

    updates

---
+++ wingbeats_fno/evaluation_wingbeats_fno.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DATASET=Wingbeats
+MODEL=fno
+TEST_SPLIT_RATIO=0.25
+BATCH_SIZE=32
+KERNEL_SIZE=11
+POOL_SIZE=2
+
+python evaluation.py --dataset $DATASET --model $MODEL --test_split_ratio $TEST_SPLIT_RATIO --batch_size $BATCH_SIZE --kernel_size $KERNEL_SIZE --pool_size $POOL_SIZE
